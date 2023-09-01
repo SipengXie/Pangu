97e0dd0079eb0fc3ed35a7800ba545c661550c5c
Hanser <1836080103@qq.com>
2023-09-01 20:37:28 +0800

    111

--- types/coverTransaction.go
+++ types/coverTransaction.go
@@ -3,11 +3,14 @@ package types
 import "math/big"
 
 type CoverTransaction struct {
-	SigAlgo   byte     // specifies the signature algorithm used
-	Signature []byte   // signature of the transaction
-	GasLimit  uint64   // gas limit of the transaction, which sums up all the logic txs
-	GasPrice  *big.Int // gas price of the transaction, determined by the CoverTxCreator
+	SigAlgo        byte                // specifies the signature algorithm used
+	Signature      []byte              // signature of the transaction
+	GasLimit       uint64              // gas limit of the transaction, which sums up all the logic txs
+	GasPrice       *big.Int            // gas price of the transaction, determined by the CoverTxCreator
+	LogicTxs       []*LogicTransaction // 聚合交易
+	EncryptionAlgo byte                // 加密算法
 
 	IsGuaranteed bool // whether the transaction is guaranteed，如果这个交易是被担保的，则任何出错回滚将触发惩罚机制
-	LogicTxs     []*LogicTransaction
+
+	Content []byte // 加密后的数据 IsGuaranteed <-> Content
 }
--- types/logicTransaction.go
+++ types/logicTransaction.go
@@ -7,16 +7,24 @@ import (
 )
 
 type LogicTransaction struct {
-	To       common.Address
-	Nonce    uint64
-	Value    *big.Int
-	GasLimit uint64
+	To             common.Address
+	Nonce          uint64
+	Value          *big.Int
+	GasLimit       uint64
+	EncryptionAlgo byte // 加密算法
 
-	SigAlgo   byte
-	Signature []byte
+	// 用户签名
+	SigAlgo         byte
+	SignatureSender []byte
+	//// 担保人签名
+	//SigGuarantee       byte
+	//SignatureGuarantee []byte
 
+	// 解密
 	Data       []byte
 	AccessList AccessList
+
+	Content []byte // 加密 Content <-> Data, AccessList
 }
 
 type LogicTransactions []*LogicTransaction
