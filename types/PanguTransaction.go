f9ab3b41c541c0cbd6b510e7a17cf942a73d0f39
SipengXie <sipengxie@buaa.edu.cn>
2023-09-02 19:24:27 +0800

    重构执行逻辑

--- types/PanguTransaction.go
+++ types/PanguTransaction.go
@@ -6,17 +6,19 @@ import (
 	"github.com/SipengXie/pangu/common"
 )
 
-type LogicTransaction struct {
+type PanguTransaction struct {
 	To       common.Address
 	Nonce    uint64
 	Value    *big.Int
 	GasLimit uint64
+	FeeCap   *big.Int
+	TipCap   *big.Int
 
 	SigAlgo   byte
 	Signature []byte
 
+	EncContent []byte // EncContent <--> {Data, AccessList}
+
 	Data       []byte
 	AccessList AccessList
 }
-
-type LogicTransactions []*LogicTransaction
