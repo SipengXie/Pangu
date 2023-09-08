fcc6e77edc4fe4b930a58ac7d2e5be99dca152ae
wenkuan Xiao <19271135@bjtu.edu.cn>
2023-09-08 14:44:53 +0800

    chore: 节点！启动！

---
+++ executor/executor_test.go
@@ -0,0 +1,15 @@
+package executor
+
+//func panguTxs(nonce uint64) {
+//	tx := &types.PanguTransaction{
+//		ChainID:    big.NewInt(1337),
+//		Nonce:      nonce,
+//		GasTipCap:  tip,
+//		GasFeeCap:  gasFee,
+//		Gas:        gasLimit,
+//		To:         nil,
+//		Value:      amount,
+//		Data:       data,
+//		AccessList: al,
+//	}
+//}
